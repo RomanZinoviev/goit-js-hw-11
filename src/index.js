ec34f856d1b6ecbc8357a4d384fc5e6d658e1900
RomanZinoviev <rzinovijev@gmail.com>
2022-02-16 07:24:47 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import './sass/main.scss';
+import axios from "axios";
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+
+const BASE_API = "https://pixabay.com/api/";
+const API_KEY = "25728701-c83c0487db4f1d7b899af3be5";

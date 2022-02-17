382a4603577e67776cbfa9f225054f05a6acd42c
RomanZinoviev <rzinovijev@gmail.com>
2022-02-17 11:35:42 +0200

    half

--- src/index.js
+++ src/index.js
@@ -3,10 +3,40 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import axios from "axios";
 import SimpleLightbox from 'simplelightbox';
+import NewApiService from './js/apiClass';
 
-const BASE_URL = "https://pixabay.com/api/";
-const URL_KEY = "25728701-c83c0487db4f1d7b899af3be5";
+const newApiService = new NewApiService;
+const searchFormEl = document.querySelector("#search-form");
+const loadMoreButton = document.querySelector(".load-more");
+const galleryEl = document.querySelector(".gallery");
 
+const searchImagesHandle = (event) => {
+    event.preventDefault();
+    newApiService.query = event.currentTarget.searchQuery.value;
+    newApiService.resetPage();
+    newApiService.fetchImages().then(images => {
+        imageMarkup(images)
+    })
+};
+
+const imageMarkup = (images) => {
+    const markup = images.map(({ webformatURL, tags, likes, views, comments, downloads }) => { 
+         return `<div class="gallery__image">
+  <img src="${webformatURL}" alt="${tags}" class="gallery__picture">
+  <p class="gallery__likes">${likes}</p>
+  <p class="gallery__views">${views}</p>
+  <p class="gallery comments">${comments}</p>
+  <p class="gallery__downoads">${downloads}</p>
+</div>`
+    }).join("");
+    galleryEl.innerHTML = markup;
+};
+
+const addImageMarkup = (images) => {
+    galleryEl.insertAdjacentHTML("beforeend", imageMarkup(images))
+}
+
+searchFormEl.addEventListener("submit", searchImagesHandle)
 
 
 
---
+++ src/js/apiClass.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export default class NewApiService {
+    constructor() {
+        this.searchQwery = "";
+        this.page = 1;
+    }
+
+    fetchImages = async () => {
+    const BASE_URL = "https://pixabay.com/api/";
+    const API_KEY = "25728701-c83c0487db4f1d7b899af3be5";
+    const saerchParameters = "image_type=photo&orientation=horizontal&safesearch=true&per_page=40";
+    try {
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${this.searchQwery}&${saerchParameters}&page=${this.page}`);
+        const images = response.data.hits;
+       return images;
+        } catch (error) { console.log(error) };
+    }
+    
+    incrementPage() {
+        this.page+=1
+    }
+
+    resetPage() {
+        this.page=1
+    }
+
+    get query() {
+        return this.searchQwery;
+    }
+
+    set query(newQwery) {
+        this.searchQwery = newQwery; 
+    }
+}
+
+
+  
